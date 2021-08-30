6f4d8944c0877fbd65a586f790e06c8aef863a22
Claudio Lespier <claudiolespier@Claudios-MacBook-Air.local>
2021-08-29 23:34:27 -0400

    Added Fav-Links

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks =[
+    {name: 'Google',
+    url: 'google.com'},
+    {name: 'Bing',
+    url: 'bing.com'}
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
